88136d848cfa8ca005231f894b33705135fa0b4e
Luca Warmenhoven <lucawarmenhoven@outlook.com>
2026-02-03 09:07:05 +0100

    Added additional utility functions for optional type wrapping

--- packages/compiler/src/ast/optionals.cpp
+++ packages/compiler/src/ast/optionals.cpp
@@ -21,7 +21,7 @@ bool stride::ast::is_optional_wrapped_type(const llvm::Type* type)
     return false;
 }
 
-llvm::Value* stride::ast::wrap_into_optional_value(
+llvm::Value* stride::ast::wrap_optional_value(
     llvm::Value* value,
     llvm::Type* optional_ty,
     llvm::IRBuilder<>* builder
@@ -86,6 +86,92 @@ llvm::Value* stride::ast::wrap_into_optional_value(
     return wrapped;
 }
 
+llvm::Value* optionally_upcast_type(llvm::Value* value, llvm::Type* target_ty, llvm::IRBuilder<>* builder)
+{
+    const auto value_ty = value->getType();
+
+    if (value_ty == target_ty)
+    {
+        return value;
+    }
+    if (value_ty->isIntegerTy() && target_ty->isIntegerTy())
+    {
+        return builder->CreateIntCast(value, target_ty, true);
+    }
+
+    if (value_ty->isFloatTy() && target_ty->isFloatTy())
+    {
+        return builder->CreateFPExt(value, target_ty, "fpext");
+    }
+
+    if (value_ty->isPointerTy() && target_ty->isPointerTy())
+    {
+        return builder->CreatePointerCast(value, target_ty);
+    }
+
+
+    return value;
+}
+
+llvm::Value* stride::ast::wrap_optional_value_gep(
+    llvm::Value* value,
+    llvm::Type* optional_ty,
+    const llvm::Module* module,
+    llvm::IRBuilder<>* builder
+)
+{
+    const auto value_ty = value->getType();
+
+    if (!is_optional_wrapped_type(optional_ty) || is_optional_wrapped_type(value_ty))
+    {
+        throw std::runtime_error("Invalid optional type");
+    }
+
+    llvm::AllocaInst* alloca = builder->CreateAlloca(optional_ty, nullptr, "optional_value_gep");
+
+    // If both types are the same, we can store directly.
+    if (value_ty == optional_ty)
+    {
+        builder->CreateStore(optionally_upcast_type(value, optional_ty, builder), alloca);
+        return alloca;
+    }
+
+    auto& ctx = module->getContext();
+
+    llvm::Value* opt_has_value = nullptr;
+    llvm::Value* opt_value = nullptr;
+
+    const auto optional_value_ty = optional_ty->getStructElementType(OPT_IDX_ELEMENT_TYPE);
+
+    if (llvm::isa<llvm::ConstantPointerNull>(value))
+    {
+        opt_has_value = llvm::ConstantInt::get(llvm::Type::getInt1Ty(ctx), OPT_NO_VALUE);
+        opt_value = llvm::UndefValue::get(optional_ty);
+    }
+    else
+    {
+        opt_has_value = llvm::ConstantInt::get(llvm::Type::getInt1Ty(ctx), OPT_HAS_VALUE);
+        opt_value = optionally_upcast_type(value, optional_value_ty, builder);
+    }
+
+    llvm::Value* has_value_ptr = builder->CreateStructGEP(
+        alloca->getAllocatedType(),
+        alloca,
+        OPT_IDX_HAS_VALUE
+    );
+    builder->CreateStore(opt_has_value, has_value_ptr);
+
+    // Store value
+    llvm::Value* value_ptr = builder->CreateStructGEP(
+        alloca->getAllocatedType(),
+        alloca,
+        OPT_IDX_ELEMENT_TYPE
+    );
+    builder->CreateStore(opt_value, value_ptr);
+
+    return alloca;
+}
+
 /// Extracts the second element from the wrapped type
 /// If it's not wrapped, we just return the value as-is.
 llvm::Value* stride::ast::unwrap_optional_value(llvm::Value* value, llvm::IRBuilder<>* builder)
@@ -99,6 +185,39 @@ llvm::Value* stride::ast::unwrap_optional_value(llvm::Value* value, llvm::IRBuil
     return builder->CreateExtractValue(
         value,
         {OPT_IDX_ELEMENT_TYPE},
-        "unwrap_optional_ret"
+        "unwrap_optional_val"
+    );
+}
+
+std::optional<bool> stride::ast::extract_has_value_from_optional(
+    llvm::Value* optional,
+    llvm::IRBuilder<>* builder
+)
+{
+    if (!optional || !is_optional_wrapped_type(optional->getType()))
+    {
+        return std::nullopt;
+    }
+
+
+    return builder->CreateExtractValue(
+        optional,
+        {OPT_IDX_HAS_VALUE},
+        "unwrap_optional_state"
+    );
+}
+
+llvm::Value* stride::ast::set_has_value_in_optional_gep(
+    llvm::Value* optional,
+    llvm::IRBuilder<>* builder,
+    const bool has_value
+)
+{
+    if (!is_optional_wrapped_type(optional->getType())) return optional;
+
+    return builder->CreateInsertValue(
+        optional,
+        builder->getInt1(has_value),
+        {OPT_IDX_HAS_VALUE}
     );
 }
