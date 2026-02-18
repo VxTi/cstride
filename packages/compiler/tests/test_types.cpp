edc60915d34d8a16b4b8503a6bfb4b7435e0d606
Luca Warmenhoven <lucawarmenhoven@outlook.com>
2026-02-18 18:01:30 +0100

    Added multiple struct type tests

--- packages/compiler/tests/test_types.cpp
+++ packages/compiler/tests/test_types.cpp
@@ -3,14 +3,47 @@
 
 using namespace stride::tests;
 
-TEST(VariableInitType, TypeMismatch)
+TEST(Types, VariableInitTypeMismatch)
 {
     assert_throws_message(R"(
         const b: i32 = 10l;
-    )", "expected type 'i32', got 'i64'");
+    )", "Type mismatch in variable declaration; expected type 'i32', got 'i64'");
 }
 
-TEST(StructType, TypeMismatch)
+TEST(Types, StructTypeMismatch)
+{
+    assert_throws_message(R"(
+        struct Point {
+            x: i32;
+            y: i32;
+        }
+
+        const a: Point = Point::{ x: 1.0D, y: 1 };
+    )", "Type mismatch for member 'x' in struct initializer 'Point': expected 'i32', got 'f64'");
+}
+
+TEST(Types, StructTypeMemberCountMismatch)
+{
+    assert_throws_message(R"(
+        struct Point {
+            x: i32;
+            y: i32;
+        }
+
+        const a: Point = Point::{ x: 1, y: 1, z: 2 };
+    )", "Too many members found in struct 'Point': expected 2, got 3");
+
+    assert_throws_message(R"(
+        struct Point {
+            x: i32;
+            y: i32;
+        }
+
+        const a: Point = Point::{ x: 1 };
+    )", "Too few members found in struct 'Point': expected 2, got 1");
+}
+
+TEST(Types, StructMemberTypeMismatch)
 {
     assert_throws_message(R"(
         struct Point {
@@ -19,19 +52,34 @@ TEST(StructType, TypeMismatch)
         }
 
         struct Color {
+            r: i32;
+            g: i32;
+            b: i32;
+        }
+
+        const a: Point = Color::{ r: 1, g: 2, b: 3 };
+    )", "Type mismatch in variable declaration; expected type 'Point', got 'Color'");
+}
+
+TEST(Types, StructReferenceTypeMismatch)
+{
+    assert_throws_message(R"(
+        struct Point {
             x: i32;
             y: i32;
         }
 
-        const a: Point = Color::{ x: 1, y: 2 };
-    )", "expected type 'Point', got 'Color'");
+        struct Vec = Point;
+
+        const a: Point = Vec::{ x: 1, y: 2 };
+    )", "Type mismatch in variable declaration; expected type 'Point', got 'Vec'");
 }
 
-TEST(ArrayType, TypeMismatch)
+TEST(Types, ArrayTypeMismatch)
 {
     assert_throws_message(R"(
         let a: i32[] = [1L, 2L, 3L];
-    )", "expected type 'Array<i32>', got 'Array<i64>'");
+    )", "Type mismatch in variable declaration; expected type 'Array<i32>', got 'Array<i64>'");
 }
 
 /*
