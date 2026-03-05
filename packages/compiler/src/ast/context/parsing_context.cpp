dbeddedb7ac77b6aaa5080628519e87730e93cf0
Luca Warmenhoven <lucawarmenhoven@outlook.com>
2026-03-05 09:00:52 +0100

    Separated field related registration / lookup functions into separate file

--- packages/compiler/src/ast/context/parsing_context.cpp
+++ packages/compiler/src/ast/context/parsing_context.cpp
@@ -44,25 +44,6 @@ void ParsingContext::define_symbol(const Symbol& symbol_name, const SymbolType t
     );
 }
 
-const FieldDefinition* ParsingContext::get_variable_def(
-    const std::string& variable_name,
-    const bool use_raw_name) const
-{
-    for (const auto& symbol_def : this->_symbols)
-    {
-        if (const auto* field_definition = dynamic_cast<const FieldDefinition*>(
-            symbol_def.get()))
-        {
-            if (field_definition->get_internal_symbol_name() == variable_name
-                || (use_raw_name && field_definition->get_field_name() == variable_name))
-            {
-                return field_definition;
-            }
-        }
-    }
-    return nullptr;
-}
-
 const IdentifiableSymbolDef* ParsingContext::get_symbol_def(
     const std::string& symbol_name) const
 {
@@ -174,82 +155,6 @@ IDefinition* ParsingContext::fuzzy_find(const std::string& symbol_name) const
     return nullptr;
 }
 
-bool ParsingContext::is_field_defined_in_scope(
-    const std::string& variable_name) const
-{
-    return std::ranges::any_of(
-        this->_symbols,
-        [&](const auto& symbol_def)
-        {
-            if (const auto* var_def = dynamic_cast<const FieldDefinition*>(symbol_def.
-                get()))
-            {
-                return var_def->get_internal_symbol_name() == variable_name;
-            }
-            return false;
-        });
-}
-
-bool ParsingContext::is_field_defined_globally(
-    const std::string& field_name) const
-{
-    auto current = this;
-    while (current != nullptr)
-    {
-        if (current->is_field_defined_in_scope(field_name))
-        {
-            return true;
-        }
-        current = current->_parent_registry.get();
-    }
-    return false;
-}
-
-void ParsingContext::define_variable_globally(
-    Symbol variable_symbol,
-    std::unique_ptr<IAstType> type
-) const
-{
-    if (is_field_defined_globally(variable_symbol.internal_name))
-    {
-        throw parsing_error(
-            ErrorType::SEMANTIC_ERROR,
-            std::format("Variable '{}' is already defined in global scope", variable_symbol.name),
-            type->get_source_fragment()
-        );
-    }
-
-    auto& global_scope = const_cast<ParsingContext&>(this->traverse_to_root());
-    global_scope._symbols.push_back(
-        std::make_unique<FieldDefinition>(
-            std::move(variable_symbol),
-            std::move(type)
-        )
-    );
-}
-
-void ParsingContext::define_variable(
-    Symbol variable_sym,
-    std::unique_ptr<IAstType> type
-)
-{
-    if (this->is_global_scope())
-    {
-        this->define_variable_globally(std::move(variable_sym), std::move(type));
-        return;
-    }
-
-    if (is_field_defined_in_scope(variable_sym.internal_name))
-    {
-        throw parsing_error(
-            ErrorType::SEMANTIC_ERROR,
-            std::format("Variable '{}' is already defined in this scope", variable_sym.name),
-            type->get_source_fragment());
-    }
-
-    this->_symbols.push_back(std::make_unique<FieldDefinition>(std::move(variable_sym), std::move(type)));
-}
-
 IDefinition* ParsingContext::lookup_symbol(const std::string& symbol_name) const
 {
     auto current = this;
@@ -261,24 +166,6 @@ IDefinition* ParsingContext::lookup_symbol(const std::string& symbol_name) const
             {
                 return definition.get();
             }
-        }
-        current = current->_parent_registry.get();
-    }
-    return nullptr;
-}
-
-const FieldDefinition* ParsingContext::lookup_variable(
-    const std::string& name,
-    const bool use_raw_name
-)
-const
-{
-    auto current = this;
-    while (current != nullptr)
-    {
-        if (const auto def = current->get_variable_def(name, use_raw_name))
-        {
-            return def;
         }
         current = current->_parent_registry.get();
     }
