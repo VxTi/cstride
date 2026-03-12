d874c5d01998c68ad077da8a1cf129da8c6a5fa4
Luca Warmenhoven <lucawarmenhoven@outlook.com>
2026-03-12 19:44:52 +0100

    Rename scripts and enhance test execution output for better clarity

---
+++ scripts/run-tests.sh
@@ -0,0 +1,22 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_ROOT="$( cd "$SCRIPT_DIR/.." && pwd )"
+
+BLUE="\033[34m"
+WHITE="\033[37m"
+RESET="\033[0m"
+
+printf "${BLUE}+---------------------------------------+${RESET}\n"
+printf "${BLUE}|                                       |${RESET}\n"
+printf "${BLUE}| ${WHITE}Running tests for Stride compiler...  ${BLUE}|${RESET}\n"
+printf "${BLUE}|                                       |${RESET}\n"
+printf "${BLUE}+---------------------------------------+${RESET}\n"
+
+cmake --build "${PROJECT_ROOT}/packages/compiler/cmake-build-debug" --target cstride_tests && "${PROJECT_ROOT}/packages/compiler/cmake-build-debug/tests/cstride_tests"
+
+printf "${BLUE}+----------------------------------------------+${RESET}\n"
+printf "${BLUE}|                                              |${RESET}\n"
+printf "${BLUE}| ${WHITE}Running tests for stride-plugin-intellij...  ${BLUE}|${RESET}\n"
+printf "${BLUE}|                                              |${RESET}\n"
+printf "${BLUE}+----------------------------------------------+${RESET}\n"
+
+cd "${PROJECT_ROOT}/packages/stride-plugin-intellij"; ./gradlew :test
--- scripts/run_tests.sh
+++
@@ -1,2 +0,0 @@
-cmake --build ./packages/compiler/cmake-build-debug --target cstride_tests &&
-./packages/compiler/cmake-build-debug/cstride_tests
